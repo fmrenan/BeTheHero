87316823dcfd70ec3208c6e9755a24b96e016fc7
Renan Muniz <fmrenan@outlook.com>
2020-03-28 15:00:32 -0300

    Initial commit

---
+++ backend/src/database/conection.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+const conf = require('../../knexfile');
+
+const config = process.env.NODE_ENV === 'test' ? conf.test : conf.development;
+
+const connection = knex(config);
+
+module.exports = connection;
---
+++ mobile/src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+export default function Routes(){
+  return(
+    <NavigationContainer>
+      <AppStack.Navigator
+        screenOptions={{ headerShown: false }}
+      >
+        
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen 
+          name="Detail" 
+          component={Detail} 
+          options={{
+            gestureEnabled: true,
+            cardOverlayEnabled: true,
+            ...TransitionPresets.SlideFromRightIOS,
+          }}
+        />
+      
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
